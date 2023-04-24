0ea4878a1e44f094f22a25a202bc93d955be9a05
Jaidenmagnan <84941950+Jaidenmagnan@users.noreply.github.com>
2023-04-24 18:17:07 -0400

    rendering the video onto the maze

--- scripts/render.py
+++ scripts/render.py
@@ -14,22 +14,21 @@ class MyApp(ShowBase):
         terrain = GeoMipTerrain("Maze")
         terrain.setBlockSize(32)
         terrain.getRoot().setSz(10) # vertical size
-        terrain.setHeightfield("../generated/maze_gray.png")
+        terrain.setHeightfield("generated/maze_gray.png")
         terrain.setBruteforce(True) # non-dynamic maze
         terrain.getRoot().reparentTo(render)
         terrain.generate()
 
         # loads colored terrain into a texture
-        terrain_texture = loader.loadTexture("../generated/maze.png")
-        terrain_texture_stage = TextureStage("terrain_texture_stage")
-        terrain_texture_stage.setMode(TextureStage.MReplace)
+        video_texture = loader.loadTexture("generated/maze.mp4")
+        video_texture_stage = TextureStage("video_texture_stage")
+        video_texture_stage.setMode(TextureStage.MReplace)
 
         # sets the colored terrain to the map
-        terrain.getRoot().setTexture(terrain_texture_stage, terrain_texture)
-        terrain.getRoot().setTexScale(terrain_texture_stage, 1, 1)
+        terrain.getRoot().setTexture(video_texture_stage, video_texture)
+        terrain.getRoot().setTexScale(video_texture_stage, 317/513, 317/513)
 
         
-        camera.setPos(-1000, 0, 0)
 
 
 app = MyApp()
