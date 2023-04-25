4a123c10e2ec2cbd6a1f365c00fcc3c79e737a70
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2023-04-25 10:31:45 -0400

    add mouse controls

--- scripts/render.py
+++ scripts/render.py
@@ -1,8 +1,48 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
-
+import numpy as np
 
 class MyApp(ShowBase):
+    
+    def setupCameraControls(self, props: WindowProperties):
+        # create reasonable camera controls
+        self.disableMouse()
+        
+        # Get the current keyboard layout.
+        # This may be a somewhat expensive operation, so don't call
+        # it all the time, instead storing the result when possible.
+        keyboard_map = self.win.get_keyboard_map()
+
+        # Find out which virtual keys are associated with the ANSI US "wasdqe"
+        w_button = keyboard_map.get_mapped_button("w")
+        a_button = keyboard_map.get_mapped_button("a")
+        s_button = keyboard_map.get_mapped_button("s")
+        d_button = keyboard_map.get_mapped_button("d")
+        q_button = keyboard_map.get_mapped_button("q")
+        e_button = keyboard_map.get_mapped_button("e")
+        
+        SPEED = 50
+        def move_camera(direction):
+            self.camera.setPos(self.camera.getPos() + render.getRelativeVector(camera, tuple(np.array(direction) * SPEED * globalClock.getDt())))
+        
+        self.accept(f"{w_button}-repeat", lambda: move_camera((0, 1, 0)))
+        self.accept(f"{a_button}-repeat", lambda: move_camera((-1, 0, 0)))
+        self.accept(f"{s_button}-repeat", lambda: move_camera((0, -1, 0)))
+        self.accept(f"{d_button}-repeat", lambda: move_camera((1, 0, 0)))
+        self.accept(f"{q_button}-repeat", lambda: move_camera((0, 0, -1)))
+        self.accept(f"{e_button}-repeat", lambda: move_camera((0, 0, 1)))
+
+        def rotate_camera(task):
+            if self.mouseWatcherNode.hasMouse():
+                x = self.mouseWatcherNode.getMouseX()
+                y = self.mouseWatcherNode.getMouseY()
+                self.camera.setH(self.camera.getH() - 100 * x)
+                self.camera.setP(self.camera.getP() + 100 * y)
+                self.win.movePointer(0, props.getXSize() // 2, props.getYSize() // 2)
+            return task.cont
+        
+        self.taskMgr.add(rotate_camera, "rotateCamera")
+        
 
     # imports a texture with specified mode ("ADD" or "REPLACE" or "DECAL"))
     def importTexture(self, path: str, mode: str, scale: float):
@@ -42,9 +82,22 @@ class MyApp(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
-        # sets window size
+        
+        # exit on escape
+        self.accept("escape", self.userExit)
+        
+
         props = WindowProperties()
+        
+        # set relative mode and hide the cursor
+        props.setCursorHidden(True)
+        props.setMouseMode(WindowProperties.M_relative)
+        # sets window size
         props.setSize(1280, 720)
+        
+        self.setupCameraControls(props)
+        
+        self.win.requestProperties(props)
 
         # coords for the card
         left, right, top, bottom = -1.7, -.3, .95, .5
@@ -60,7 +113,6 @@ class MyApp(ShowBase):
         self.addText("Breadth First Search: ", (-1.7, 0, .65), (255, 0, 0, 1))
         self.addText("Dijkstra's: ", (-1.7, 0, .85), (0, 0, 255, 1))
 
-        self.win.requestProperties(props)
         self.terrain = None  # initializes terrain to none
         self.initiateHeightMap("generated/maze_gray.png", 32, 20)  # creates terrain
         self.importTexture("generated/maze.mp4", "REPLACE", 317/513)  # imports video onto map
